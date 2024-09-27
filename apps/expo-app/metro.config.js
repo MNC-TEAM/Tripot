5dc01f57fab68dda0c3b5756d05bfb7a5f031585
Kim Ji Yu <spbabo97@naver.com>
2024-09-27 21:44:51 +0900

    chore : expo lint설정

--- apps/expo-app/metro.config.js
+++ apps/expo-app/metro.config.js
@@ -1,19 +1,23 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+/* eslint-env node */
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 // Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, "../..");
 
-const config = getDefaultConfig(projectRoot);
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(monorepoRoot, "node_modules"),
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
 ];
 
 module.exports = config;
