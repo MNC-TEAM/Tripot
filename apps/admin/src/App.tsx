85dac51c34edb56dac0dca18ca293dfe87c74ac6
Kim Ji Yu <spbabo97@naver.com>
2024-09-27 20:39:58 +0900

    feat : 대시보드 기본 UI

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,11 +1,32 @@
-import { Layout } from "antd";
+import { Avatar, Layout, Menu, MenuProps } from "antd";
+import { Typography } from "antd";
+import {
+  IoAppsSharp,
+  IoChatbubblesOutline,
+  IoPeopleSharp,
+  IoSettingsSharp,
+} from "react-icons/io5";
 
 const App = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider width="12.5%" style={{ background: "#eee" }} />
+      <Sider width="200" style={{ background: "#fff" }}>
+        <Title level={4} style={{ textAlign: "center" }}>
+          Logo
+        </Title>
+        <Menu mode="inline" items={items} style={{ marginTop: 24 }} />
+      </Sider>
       <Layout>
-        <Header style={{ background: "#acf" }}>어드민</Header>
+        <Header
+          style={{
+            background: "#acf",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Avatar>U</Avatar>
+        </Header>
         <Content style={{ padding: 24 }}>asdsad</Content>
         <Footer
           style={{ textAlign: "center", fontSize: 12, background: "#eee" }}
@@ -19,4 +40,31 @@ const App = () => {
 
 export default App;
 
+type MenuItem = Required<MenuProps>["items"][number];
+
 const { Header, Sider, Content, Footer } = Layout;
+
+const { Title } = Typography;
+
+const items: MenuItem[] = [
+  {
+    key: "sub1",
+    label: "Dashboard",
+    icon: <IoAppsSharp />,
+  },
+  {
+    key: "sub2",
+    label: "Story",
+    icon: <IoChatbubblesOutline />,
+  },
+  {
+    key: "sub3",
+    label: "User",
+    icon: <IoPeopleSharp />,
+  },
+  {
+    key: "sub4",
+    label: "Settings",
+    icon: <IoSettingsSharp />,
+  },
+];
