f15b5b25588b197e34bbf7397281fc360bd23467
Kim Ji Yu <spbabo97@naver.com>
2024-09-27 23:45:51 +0900

    feat : MapView, Maker 생성

--- apps/expo-app/app/index.tsx
+++ apps/expo-app/app/index.tsx
@@ -1,5 +1,94 @@
-import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
+import * as Location from 'expo-location';
+import styled from 'styled-components/native';
 
 export default function Page() {
-  return <Text>Top-level page</Text>;
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <MapContainer>
+      <MapContainer>
+        <StyledMapView>
+          {location && (
+            <>
+              <Marker coordinate={location.coords}>
+                <Marker2>
+                  <MarkerImage
+                    source={{ uri: 'https://picsum.photos/200' }}
+                    resizeMode="cover"
+                  />
+                </Marker2>
+              </Marker>
+              <Marker
+                coordinate={{
+                  latitude: location.coords.latitude + 1,
+                  longitude: location.coords.longitude + 1,
+                }}
+              >
+                <Marker2>
+                  <MarkerImage
+                    source={{ uri: 'https://picsum.photos/200' }}
+                    resizeMode="cover"
+                  />
+                </Marker2>
+              </Marker>
+              <Marker
+                coordinate={{
+                  latitude: location.coords.latitude - 1,
+                  longitude: location.coords.longitude - 1,
+                }}
+              >
+                <Marker2>
+                  <MarkerImage
+                    source={{ uri: 'https://picsum.photos/200' }}
+                    resizeMode="cover"
+                  />
+                </Marker2>
+              </Marker>
+            </>
+          )}
+        </StyledMapView>
+      </MapContainer>
+    </MapContainer>
+  );
 }
+
+const MapContainer = styled.View`
+  flex: 1;
+`;
+
+const StyledMapView = styled(MapView)`
+  flex: 1;
+`;
+
+const Marker2 = styled.View`
+  width: 67px;
+  height: 67px;
+  border-radius: 6px;
+  border-width: 8px;
+  border-color: #fff;
+  box-sizing: border-box;
+`;
+
+const MarkerImage = styled.Image`
+  flex: 1;
+  justify-content: 'center';
+  border-radius: 6px;
+`;
