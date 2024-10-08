e8b3b94beac8d554f383df0cb391530a7f044702
Kim Ji Yu <spbabo97@naver.com>
2024-10-09 07:44:04 +0900

    feat : eslint 설정변경

--- apps/expo-app/.eslintrc.js
+++ apps/expo-app/.eslintrc.js
@@ -3,5 +3,13 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'import/order': 'off',
+    '@typescript-eslint/ban-types': 'off', // 뜨는 이유를 모르겠어서 일단 off
+    'import/no-unresolved': 'off', // 경로 오류 임시방편
+    'react/self-closing-comp': 'error', // 빈 컴포넌트는 자동으로 닫히도록 에러로 처리
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
+    ], // 불필요한 중괄호 사용 금지
   },
 };
